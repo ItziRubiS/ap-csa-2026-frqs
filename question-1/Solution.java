3fe03f00bf32ce65f2368413a4b83218427ab536
ItziRubiS. <saavedraitzi@gmail.com>
2026-05-20 11:51:12 -0400

    Update Solution.java

--- question-1/Solution.java
+++ question-1/Solution.java
@@ -1,1 +1,9 @@
-
+public Account(String requestedName){
+  String newUser= requestedName;
+  int num=1;
+  while(!isAvailable(newUser)){
+   newUser=requestedName+num;
+    num++;
+  }
+  return newUser;
+}
