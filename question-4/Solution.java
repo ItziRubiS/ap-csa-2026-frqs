47dfe5c9c05e68e62e818dd8571d5bc98ab6b3c9
ItziRubiS. <saavedraitzi@gmail.com>
2026-06-04 18:38:30 -0400

    Update Solution.java

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,4 +1,10 @@
 public int getPointsForRow(int targetRow)
 { 
- 
+ int sum=0;
+   for (int col = 0; col < board[targetRow].length; col++)
+    {
+        sum += board[targetRow][col].getPoints();
+    }
+
+    return 2 * sum;
   }
