c8170c1d12e9576cf27df4a7edf53eea9403787a
ItziRubiS. <saavedraitzi@gmail.com>
2026-06-04 11:47:34 -0400

    Update Solution.java

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,1 +1,16 @@
-
+public class Bottle
+  {
+    private double capacity;
+    private double amount;
+public Bottle (double c){
+  capacity=c*0.25;
+  amount= c;
+  }
+     public double updateAmount(double r){
+       amount-=r
+         if (amount<capacity){
+          amount=capacity
+         }
+       return amount;
+     }
+  }
