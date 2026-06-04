133dcd149309aed346d7b9daea02925fc1b3a375
ItziRubiS. <saavedraitzi@gmail.com>
2026-06-04 16:54:41 -0400

    Update Solution.java

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,4 +1,19 @@
 public int moreHistoryThanMathAbsences()
 { 
-  
+  int count = 0;
+
+    for (CourseRecord history : historyList)
+    {
+        for (CourseRecord math : mathList)
+        {
+            if (history.getStudentID().equals(math.getStudentID()))
+            {
+                if (history.getAbsences() > math.getAbsences())
+                {
+                    count++;
+                }
+            }
+        }
+    }
+
 }
